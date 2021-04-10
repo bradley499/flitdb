cfbf439be1c41415d95ac0de4f1b468bee98afa2
Bradley Marshall <bradm499@gmail.com>
2021-04-10 15:37:02 +0100

    Added more tests

---
+++ tests/sequential_deletions.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+#include <flit.h>
+
+#define MAX 999
+#define MAX_INSERTIONS (MAX * MAX)
+
+int main(int argc, char const *argv[])
+{
+	flitdb *flit;
+	assert(flitdb_setup("./test2.db", &flit, FLITDB_CREATE) == FLITDB_SUCCESS);
+	for (unsigned short x = 0; x < MAX; x++)
+	{
+		for (unsigned short y = 0; y < MAX; y++)
+		{
+			assert(flitdb_delete(&flit, (x + 1), (y + 1)) == FLITDB_DONE);
+		}
+	}
+	flitdb_close(&flit);
+	return 0;
+}
---
+++ tests/sequential_inserts.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+#include <flit.h>
+
+#define MAX 999
+#define MAX_INSERTIONS (MAX * MAX)
+#define MAX_VALUE 99999
+
+int main(int argc, char const *argv[])
+{
+	flitdb *flit;
+	assert(flitdb_setup("./test2.db", &flit, FLITDB_CREATE) == FLITDB_SUCCESS);
+	unsigned int inserted[MAX][MAX] = {{}};
+	for (unsigned short x = 0; x < MAX; x++)
+	{
+		for (unsigned short y = 0; y < MAX; y++)
+		{
+			inserted[x][y] = rand() % MAX_VALUE;
+			assert(flitdb_insert_int(&flit, (x + 1), (y + 1), inserted[x][y]) == FLITDB_DONE);
+		}
+	}
+	for (unsigned short x = 0; x < MAX; x++)
+	{
+		for (unsigned short y = 0; y < MAX; y++)
+		{
+			flitdb_extract(&flit, (x + 1), (y + 1));
+			assert(flitdb_retrieve_int(&flit) == inserted[x][y]);	
+		}
+	}
+	flitdb_close(&flit);
+	return 0;
+}
