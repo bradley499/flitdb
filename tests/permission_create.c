f07f2254dae02d293505dde23a40f29b11c2189e
Bradley Marshall <bradm499@gmail.com>
2021-04-26 10:18:57 +0100

    Added permission tests

---
+++ tests/permission_create.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+#include <flit.h>
+
+
+int main(int argc, char const *argv[])
+{
+	flitdb *flit;
+	assert(flitdb_setup("./test_create.db", &flit, FLITDB_READONLY) == FLITDB_NOT_FOUND);
+	flitdb_close(&flit);
+	assert(flitdb_setup("./test_create.db", &flit, FLITDB_CREATE) == FLITDB_SUCCESS);
+	flitdb_close(&flit);
+	return 0;
+}
---
+++ tests/permission_readonly.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+#include <flit.h>
+
+
+int main(int argc, char const *argv[])
+{
+	flitdb *flit;
+	assert(flitdb_setup("./test_readonly.db", &flit, FLITDB_READONLY) == FLITDB_NOT_FOUND);
+	flitdb_close(&flit);
+	FILE *readonly_db;
+	readonly_db = fopen("./test_readonly.db", "w"); // create empty database
+	fclose(readonly_db);
+	assert(flitdb_setup("./test_readonly.db", &flit, FLITDB_READONLY) == FLITDB_SUCCESS);
+	assert(flitdb_insert_int(&flit, 1, 1, 12345) == FLITDB_READONLY);
+	flitdb_close(&flit);
+	return 0;
+}
